3489be913868a8de8648f1729645e3ab2bf293c8
OptimusPrime <gulliverraed@gmail.com>
2024-11-20 16:35:33 +0200

    completed the Xylophoone app, when you press on each button the specified note plays.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: (sender.titleLabel?.text)!)
+        UIView.animate(withDuration: 0.2, animations:{
+            sender.alpha = 0.5})
+        { _ in
+            UIView.animate(withDuration: 0.2, animations: {sender.alpha = 1.0})
+        }
+        
+    }
+    
+    func playSound(soundName : String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
